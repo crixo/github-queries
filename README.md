b65fa417da662b23609b64b66c150c3558f819fa
crixo <cristiano.deg@gmail.com>
2021-11-13 20:39:39 +0100

    basic search user

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # github-queries
+
+## Dependencies
+
+- [Golang](https://golang.org/doc/install)
+
+## Libraries
+
+- [go-github](https://github.com/google/go-github)
--- main.go
+++ main.go
@@ -11,8 +11,11 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
 
-	"github.com/google/go-github/v39/github"
+	"github.com/google/go-github/v40/github"
+
+	"golang.org/x/oauth2"
 )
 
 // Fetch all the public organizations' membership of a user.
@@ -23,18 +26,61 @@ func fetchOrganizations(username string) ([]*github.Repository, error) {
 	return orgs, err
 }
 
+// func fetchUsers(username string) ([]*github.User, error) {
+// 	client := github.NewClient(nil)
+// 	opts :=
+// 	users, _, err := client.Users.ListAll(context.Background(), opts)
+// 	return users, err
+// }
+
 func main() {
-	var username string
-	fmt.Print("Enter GitHub username: ")
-	fmt.Scanf("%s", &username)
+	//fmt.Print("Enter User Location : ")
+	//var input string
+	// fmt.Scanln(&input)
+	// fmt.Print(input)
+	//input = "Italy"
 
-	organizations, err := fetchOrganizations(username)
-	if err != nil {
-		fmt.Printf("Error: %v\n", err)
-		return
+	ctx := context.Background()
+
+	var token string = os.Getenv("PAT")
+
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: token},
+	)
+
+	tc := oauth2.NewClient(ctx, ts)
+
+	client := github.NewClient(tc)
+
+	opts := &github.SearchOptions{
+		ListOptions: github.ListOptions{PerPage: 20},
 	}
+	// get all pages of results
+	var allUsers []*github.User
+	for {
 
-	for i, organization := range organizations {
-		fmt.Printf("%v. %s\n", i+1, *organization.Name)
+		// if lastId > 0 {
+		// 	opt.Since = int64(lastId)
+		// }
+
+		// https://docs.github.com/en/rest/reference/search#search-users
+		query := "language:go location:italy"
+
+		userSearch, resp, err := client.Search.Users(ctx, query, opts)
+		if err != nil {
+			//return err
+		}
+
+		for _, user := range userSearch.Users {
+			allUsers = append(allUsers, user)
+			fmt.Println(*user.Login)
+
+			//lastId = int(*user.ID)
+		}
+
+		if resp.NextPage == 0 {
+			break
+		}
+		opts.Page = resp.NextPage
 	}
 }
